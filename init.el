d62ee4f34cb7dcd9cb2a07fb33410c32c65cca20
G P <giulio.pietroiusti@gmail.com>
2018-02-23 19:41:45 +0100

    Config packages in init.el

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
