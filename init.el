190f3ed1fd8763115f3bb4d44699810d8e0975ad
gp <giulio.pietroiusti@gmail.com>
2018-12-31 15:23:58 +0100

    exwm

--- init.el
+++ init.el
@@ -103,4 +103,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "unspecified-bg")))))
+ '(fringe ((t (:background "#002b36")))))
