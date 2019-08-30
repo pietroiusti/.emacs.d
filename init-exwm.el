986abbe4c3119d00079a74e920667bb6f412ebb1
glptrst <giulio.pietroiusti@gmail.com>
2019-08-30 12:20:45 +0200

    EXWM

--- init-exwm.el
+++ init-exwm.el
@@ -1,3 +1,31 @@
+;; Disable menu-bar, tool-bar and scroll-bar to increase the usable space.
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+;; Also shrink fringes to 1 pixel.
+(fringe-mode 1)
+
+;; Turn on `display-time-mode' if you don't use an external bar.
+(setq display-time-default-load-average nil)
+(display-time-mode t)
+
+;; You are strongly encouraged to enable something like `ido-mode' to alter
+;; the default behavior of 'C-x b', or you will take great pains to switch
+;; to or back from a floating frame (remember 'C-x 5 o' if you refuse this
+;; proposal however).
+;; You may also want to call `exwm-config-ido' later (see below).
+(ido-mode 1)
+
+;; Emacs server is not required to run EXWM but it has some interesting uses
+;; (see next section).
+;(server-start)
+
+;;;; Below are configurations for EXWM.
+
+;; Add paths (not required if EXWM is installed from GNU ELPA).
+;(add-to-list 'load-path "/path/to/xelb/")
+;(add-to-list 'load-path "/path/to/exwm/")
+
 ;; Load EXWM.
 (require 'exwm)
 
@@ -6,7 +34,7 @@
 (exwm-config-ido)
 
 ;; Set the initial number of workspaces (they can also be created later).
-(setq exwm-workspace-number 4)
+(setq exwm-workspace-number 10)
 
 ;; All buffers created in EXWM mode are named "*EXWM*". You may want to
 ;; change it in `exwm-update-class-hook' and `exwm-update-title-hook', which
@@ -14,7 +42,7 @@
 ;; some advice on this topic:
 ;; + Always use `exwm-workspace-rename-buffer` to avoid naming conflict.
 ;; + For applications with multiple windows (e.g. GIMP), the class names of
-					;    all windows are probably the same.  Using window titles for them makes
+;    all windows are probably the same.  Using window titles for them makes
 ;;   more sense.
 ;; In the following example, we use class names for all windows expect for
 ;; Java applications and GIMP.
@@ -50,10 +78,14 @@
         ([?\s-&] . (lambda (command)
 		     (interactive (list (read-shell-command "$ ")))
 		     (start-process-shell-command command nil command)))
+	([?\s-p] . (lambda (command)
+		     (interactive (list (read-shell-command "$ ")))
+		     (start-process-shell-command command nil command)))
         ;; Bind "s-<f2>" to "slock", a simple X display locker.
         ([s-f2] . (lambda ()
 		    (interactive)
-		    (start-process "" nil "/usr/bin/slock")))))
+		    (start-process "" nil "/usr/bin/slock ; systemctl suspend")))))
+
 
 ;; To add a key binding only available in line-mode, simply define it in
 ;; `exwm-mode-map'.  The following example shortens 'C-c q' to 'C-q'.
@@ -95,7 +127,9 @@
 (exwm-systemtray-enable)
 
 ;; Autohide minibuffer & echo area
-(setq exwm-workspace-minibuffer-position 'bottom)
+;;(setq exwm-workspace-minibuffer-position 'bottom) ;; doesn't work well :(
+;;(setq echo-keystrokes 0)
+
 
 ;; Custom keybindings
 ;;
@@ -125,12 +159,37 @@
 		      (interactive)
 		      (shell-command "i3lock -c 000000 ; systemctl suspend")))
 
-;; set keycodes
-(start-process-shell-command "setxkbmap" nil "setxkbmap -keycodes evdev_custom_thinkpad_T460")
+;; chromium
+(exwm-input-set-key (kbd "s-c")
+		    (lambda ()
+		      (interactive)
+		      (start-process-shell-command "chromium" nil "chromium")))
 
-;; You can hide the minibuffer and echo area when they're not used, by
-;; uncommenting the following line.
-					;(setq exwm-workspace-minibuffer-position 'bottom)
+;; emacsclient
+(exwm-input-set-key (kbd "s-e")
+		    (lambda ()
+		      (interactive)
+		      (start-process-shell-command "emacsclient" nil "emacsclient -c")))
+
+(exwm-input-set-key (kbd "s-m")
+		    (lambda ()
+		      (interactive)
+		      (exwm-workspace-toggle-minibuffer)))
+
+(require 'exwm-randr)
+;; (setq exwm-randr-workspace-output-plist '(0 "DP1" 1 "DP1" 2 "DP1"
+;;  					    3 "DP1" 4 "DP1"
+;;  					    5 "DP1" 6 "DP1"
+;;  					    7 "DP1" 8 "DP1"))
+
+;; (setq exwm-randr-workspace-output-plist '(1 "HDMI2" 2 "HDMI2"
+;; 					    3 "HDMI2" 4 "HDMI2" 5 "HDMI2" 6 "HDMI2" 7 "HDMI2" 8 "HDMI2"
+;; 					    9 "HDMI2"))
+;; (add-hook 'exwm-randr-screen-change-hook
+;;           (lambda ()
+;;             (start-process-shell-command
+;;              "xrandr" nil "xrandr --output HDMI2 --left-of eDP1 --auto")))
+(exwm-randr-enable)
 
 ;; Do not forget to enable EXWM. It will start by itself when things are
 ;; ready.  You can put it _anywhere_ in your configuration.
