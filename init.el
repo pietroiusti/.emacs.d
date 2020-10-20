9c8c5130adb936237011299bc0897527a3cc25d1
glptrst <giulio.pietroiusti@gmail.com>
2020-10-20 20:51:11 +0200

    Add presentation mode (and academic phrases package)

--- init.el
+++ init.el
@@ -25,3 +25,87 @@
 (require 'use-package)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(compilation-message-face 'default)
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes '(solarized-light))
+ '(custom-safe-themes
+   '("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" default))
+ '(fci-rule-color "#eee8d5")
+ '(fido-mode t)
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
+ '(highlight-symbol-colors
+   (--map
+	(solarized-color-blend it "#fdf6e3" 0.25)
+	'("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2")))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+   '(("#eee8d5" . 0)
+	 ("#B4C342" . 20)
+	 ("#69CABF" . 30)
+	 ("#69B7F0" . 50)
+	 ("#DEB542" . 60)
+	 ("#F2804F" . 70)
+	 ("#F771AC" . 85)
+	 ("#eee8d5" . 100)))
+ '(hl-bg-colors
+   '("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342"))
+ '(hl-fg-colors
+   '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
+ '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
+ '(nrepl-message-colors
+   '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
+ '(org-agenda-files
+   '("~/Nextcloud/UNI/PhD/presentations/GRG_2020-1/presentation.org" "~/Nextcloud/org/activities.org" "~/Nextcloud/org/todo.org" "~/Nextcloud/org/meetings.org" "~/Nextcloud/org/notes.org" "~/Nextcloud/org/readings.org" "~/Nextcloud/org/teaching.org" "~/Nextcloud/org/habits.org"))
+ '(package-selected-packages
+   '(hide-mode-line org-present selectrum icomplete-vertical academic-phrases org-tree-slide exwm pug-mode js2-mode rainbow-delimiters paredit elfeed buffer-move switch-window engine-mode restclient impatient-mode avy smex dired-subtree dired-narrow web-mode which-key ido-completing-read+ auctex company pdf-tools magit evil olivetti color-theme-sanityinc-tomorrow solarized-theme doom-modeline s ibuffer-projectile use-package))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#dc322f")
+	 (40 . "#c9485ddd1797")
+	 (60 . "#bf7e73b30bcb")
+	 (80 . "#b58900")
+	 (100 . "#a5a58ee30000")
+	 (120 . "#9d9d91910000")
+	 (140 . "#9595943e0000")
+	 (160 . "#8d8d96eb0000")
+	 (180 . "#859900")
+	 (200 . "#67119c4632dd")
+	 (220 . "#57d79d9d4c4c")
+	 (240 . "#489d9ef365ba")
+	 (260 . "#3963a04a7f29")
+	 (280 . "#2aa198")
+	 (300 . "#288e98cbafe2")
+	 (320 . "#27c19460bb87")
+	 (340 . "#26f38ff5c72c")
+	 (360 . "#268bd2")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   '(unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
