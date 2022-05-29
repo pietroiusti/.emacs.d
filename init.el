eb39bdaf88e7acc7559f1a10a71100aef775630a
glptrst <giulio.pietroiusti@gmail.com>
2022-05-30 00:45:31 +0200

    Use web-mode for js, jsx and html.

--- init.el
+++ init.el
@@ -491,13 +491,20 @@
 (use-package web-mode
   :ensure t
   :config
+  (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
-  (defun my-web-mode-hook ()
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+
+  (defun web-mode-init-hook ()
     "Hooks for Web mode."
     (setq web-mode-markup-indent-offset 2)
-    )
-  (add-hook 'web-mode-hook  'my-web-mode-hook))
-
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2))
+  (add-hook 'web-mode-hook  'web-mode-init-hook))
+ 
 (use-package engine-mode
   :ensure t
   :config
